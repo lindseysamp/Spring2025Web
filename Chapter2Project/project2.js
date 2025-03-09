00ddae4dedf33d4373a6bcfefd66c94f8a9de957
lindseysamp <lindseysamp77@gmail.com>
2025-03-09 17:29:53 -0500

    Update project2.js

--- Chapter2Project/project2.js
+++ Chapter2Project/project2.js
@@ -1,1 +1,30 @@
+//Lindsey Sample
+//3.9.25
+
+let myName = "Lindsey Sample"
+let para1 = document.getElementById("p1");
+para1.textContent = myName;
+
+let n1 = 5;
+let n2 = 10;
+
+let numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+
+let numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+
+let myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+
+let myNameMultNum = myName * n1;
+document.getElementById("p5").textContent = myNameMultNum;
+
+let age = 19;
+let ageCompare = age >= (n1 * n2);
+document.getElementById("p6").textContent = ageCompare;
+
+n1 = 7
+n2 = 14
+
 
